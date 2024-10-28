ac210dc68a44a7a42e41b963d2e503befcb2ab68
Oladeji-Sayomi <oluwasayomioladeji@gmail.com>
2024-10-28 12:04:02 +0100

    (My first inmplementation of Cargo in Rust)

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'O';
+    let surname = "Oladeji";
+    let first_name = "Oluwasayomi";
+    let middle_name = "Adeolu";
+
+    println!(" ");
+    println!("Special character: {}",special_character);
+    println!("Alphabet: {}",alphabet);
+    println!("Name: {} {} {}",surname, middle_name, first_name);
+}
---
+++ week-3/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\Sayomi\Documents\s.oladejiCos101\week-3\practice_6\target\debug\practice_6.exe: C:\Users\Sayomi\Documents\s.oladejiCos101\week-3\practice_6\src\main.rs
